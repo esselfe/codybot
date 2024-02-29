563ae4c6400c5b918643e25ba532add00c7b16fd
Stephane Fontaine <esselfe16@gmail.com>
2024-02-28 21:43:51 -0500

    1.1.2: set target once

--- src/console.c
+++ src/console.c
@@ -13,6 +13,9 @@ void ConsoleReadInput(void) {
 		fgets(buffer_line, 4095, stdin);
 		char *cp;
 		cp = buffer_line;
+		
+		target = current_channel;
+		
 		// Ignore empty lines.
 		if (buffer_line[0] == '\n')
 			continue;
@@ -27,47 +30,36 @@ void ConsoleReadInput(void) {
 		}
 		else if (strcmp(buffer_line, "debug\n") == 0) {
 			sprintf(buffer, "debug = %d", debug);
-			target = current_channel;
 			Msg(buffer);
 		}
 		else if (strcmp(buffer_line, "debug on\n") == 0) {
 			debug = 1;
-			target = current_channel;
 			Msg("debug = 1");
 		}
 		else if (strcmp(buffer_line, "debug off\n") == 0) {
 			debug = 0;
-			target = current_channel;
 			Msg("debug = 0");
 		}
-		else if (strcmp(buffer_line, "fortune\n") == 0) {
-			sprintf(raw.channel, "%s", current_channel);
+		else if (strcmp(buffer_line, "fortune\n") == 0)
 			Fortune(&raw);
-		}
 		else if (strncmp(buffer_line, "msg ", 4) == 0) {
 			sprintf(buffer, "%s", buffer_line+4);
-			sprintf(raw.channel, "%s", current_channel);
-			target = raw.channel;
 			Msg(buffer);
 		}
 		else if (strcmp(buffer_line, "sh_disable\n") == 0) {
 			sh_disabled = 1;
-			target = current_channel;
 			Msg("sh_disabled = 1");
 		}
 		else if (strcmp(buffer_line, "sh_enable\n") == 0) {
 			sh_disabled = 0;
-			target = current_channel;
 			Msg("sh_disabled = 1");
 		}
 		else if (strcmp(buffer_line, "sh_lock\n") == 0) {
 			sh_locked = 1;
-			target = current_channel;
 			Msg("sh_locked = 1");
 		}
 		else if (strcmp(buffer_line, "sh_unlock\n") == 0) {
 			sh_locked = 0;
-			target = current_channel;
 			Msg("sh_locked = 0");
 		}
 		else if (strncmp(buffer_line, "id ", 3) == 0) {
@@ -95,7 +87,6 @@ void ConsoleReadInput(void) {
 		}
 		else if (strncmp(buffer_line, "timeout\n", 8) == 0) {
 			sprintf(buffer, "timeout = %d", cmd_timeout);
-			target = current_channel;
 			Msg(buffer);
 		}
 		else if (strncmp(buffer_line, "timeout ", 8) == 0) {
@@ -118,19 +109,16 @@ void ConsoleReadInput(void) {
 			if (cmd_timeout == 0)
 				cmd_timeout = 10;
 			sprintf(buffer, "timeout = %d", cmd_timeout);
-			target = current_channel;
 			Msg(buffer);
 		}
 		else if (strcmp(buffer_line, "trigger\n") == 0) {
 			sprintf(buffer, "trigger = %c", trigger_char);
-			target = current_channel;
 			Msg(buffer);
 		}
 		else if (strncmp(buffer_line, "trigger ", 8) == 0) {
 			if (*(cp+8)!='\n')
 				trigger_char = *(cp+8);
 			sprintf(buffer, "trigger = %c", trigger_char);
-			target = current_channel;
 			Msg(buffer);
 		}
 		// Otherwise send the input line directly to the server.
